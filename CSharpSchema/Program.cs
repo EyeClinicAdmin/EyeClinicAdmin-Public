a430952e802f83817c9458392e48722dd339e1ac
niels <nielsfreelancer7750@gmail.com>
2025-12-15 13:01:56 +0100

    Add csharp schema generation

---
+++ CSharpSchema/Program.cs
@@ -0,0 +1,14 @@
+﻿using NJsonSchema;
+using NJsonSchema.CodeGeneration.CSharp;
+
+var schema = await JsonSchema.FromFileAsync("../schemas/eca/v1/eyeclinic-dataset.schema.json");
+var generator = new CSharpGenerator(schema, new CSharpGeneratorSettings
+{
+    Namespace = "Namespace",
+    ClassStyle = CSharpClassStyle.Record,
+    JsonLibrary = CSharpJsonLibrary.SystemTextJson,
+    GenerateDataAnnotations = true
+});
+
+var code = generator.GenerateFile();
+File.WriteAllText("GeneratedClasses.cs", code);
